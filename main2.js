53f2f8559d2c133c036054da5a0feaa637c7d895
VirajxD0 <virajvd99@gmail.com>
2024-10-16 09:07:38 +0530

    dependencies added

--- main2.js
+++ main2.js
@@ -9,20 +9,4 @@ const canvas = document.getElementById("canvas3ds");
 const spline = new Application(canvas);
 
 // Load the Spline scene and set up the animation
-spline
-  .load("https://prod.spline.design/60bmf97LcGdPYIAR/scene.splinecode")
-  .then(() => {
-    const keyboard = app.findObjectByName("keyboard");
-
-    gsap.set(keyboard.scale, { x: 1, y: 800, z: 1 });
-    gsap.set(keyboard.position, { x: 110, y: 50 });
-
-    let rotateKeyboard = gsap.to(keyboard.rotation, {
-      y: Math.PI * 2 + keyboard.rotation.y,
-      x: 0,
-      z: 0,
-
-      repeat: -1,
-      ease: "none",
-    });
-  });
+spline.load("https://prod.spline.design/60bmf97LcGdPYIAR/scene.splinecode");
